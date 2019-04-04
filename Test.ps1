ff780e192b9d18410fb533da1de15d0de13cefdb
samanaz <saman.azadpour@hotmail.com>
2019-04-04 17:20:01 +1100

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,17 @@
-﻿Get-RIAzResourceGroupName -Region aea -System vmi -ResourceType mgm -Environment prod
+﻿
+$scriptdir = Split-Path -Parent $PSCommandPath
+$InfrastructureName = "Infrastructure-Naming.ps1"
+$InfrastructureDeployment="Infrastructure-Deployment.ps1"
+$Database="Database.ps1"
+
+$ModuleInfrastructureName = Join-Path $scriptdir $InfrastructureName
+$ModuleInfrastructureDeployment= Join-Path $scriptdir $InfrastructureDeployment
+$ModuleDatabase= Join-Path $scriptdir $Database
+Write-Output $ModuleInfrastructureName
+Write-Output $ModuleInfrastructureDeployment
+Write-Output $ModuleDatabase
+
+
+Import-Module $ModuleInfrastructureName
+Import-Module $ModuleInfrastructureDeployment
+Import-Module $ModuleDatabase
